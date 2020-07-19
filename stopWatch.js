d7e96c99f1493ad31a4ff5d1b0333260b38d865f
Kane Li <kandy2kane@bitbucket.org>
2020-07-19 08:40:05 -0400

    added condition if it's running or stopped already

--- stopWatch.js
+++ stopWatch.js
@@ -2,31 +2,56 @@
 
 // define object
 stopWatch = function(){
-    this.duration;
+    let duration;
     let startTime;
     let stopTime;
+    let running = false;
+    let currentDate;
     
     this.start = function(){
-        let currentDate = new Date();
-        startTime = currentDate.getTime();
-        console.log('Stop Watch started');
+        if (running==false) {
+            currentDate = new Date();
+            startTime = currentDate.getTime();
+            console.log('Stop Watch started');
+            running = true;
+        } 
+        else {
+            console.log('Stop watch already running');
+        }
     };
 
+
     this.stop = function(){
-        let currentDate = new Date();
-        stopTime = currentDate.getTime();
-        diff = stopTime - startTime;
-        min = Math.floor(diff/60000);
-        sec = ((diff % 60000) / 1000).toFixed(0);
-        this.duration = min + ':' + ( sec < 10 ? '0' : '') + sec;
-        console.log('Stop Watch stopped');
-        console.log('duration: ' + this.duration);
+        if (running==true) {
+            currentDate = new Date();
+            stopTime = currentDate.getTime();
+            diff = stopTime - startTime;
+            // min = Math.floor(diff/60000);
+            // sec = ((diff % 60000) / 1000).toFixed(0);
+            // duration = min + ':' + ( sec < 10 ? '0' : '') + sec;
+            duration = diff / 1000;
+            console.log('Stop Watch stopped');
+            console.log('duration: ' + duration);
+            running=false;
+        } else {
+            console.log('Stop watching is not running');
+        }
     };
 
     this.reset = function(){
-        this.duration = 0;
+        duration = 0;
         console.log('Stop watch resetted');
     };
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {
+            if (!running) {
+                return duration;
+            } else {
+                console.log('Stop watch is running');
+            };
+        }
+    });
 }
 
 const sw = new stopWatch();
